55ec27b47727fa00a8ce9ecf0d5795ea614819e7
Rumyyyyy <dkfmadl1219@naver.com>
2024-03-26 08:59:56 +0900

    git commit

---
+++ 2nd/sln_2/project_1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console; // static  ==> 정적이라는 의미 static으로 선언한 변수는 '전역변수'
+
+namespace project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)  // static이 있는 이유 : 부팅을 하게될 시 이미 메모리 공간이 할당되어 있어야 파일을 읽을 수 있는데 미리 메모리 할당을 해주기 위해 static을 붙인다.
+        {
+            System.Console.WriteLine(275); // 줄 바꿈 되는 쓰기
+            Console.WriteLine(275);
+            WriteLine(275);
+
+           System.Console.Write(275); // 줄 바꿈 없는 쓰기
+            Console.Write(275);
+            Write(275);
+            Console.WriteLine();
+
+            Console.WriteLine("C# Programing");
+
+            // 주석
+            /* 여러줄 주석 */
+
+            Console.WriteLine(4 % 3); // 1
+            Console.WriteLine("A"); // 문자열
+            Console.WriteLine('A'); // 문자
+            Console.WriteLine("안녕" + "하세요"); // 문자열 연결 연산자
+            Console.WriteLine('a' + 'b');  // 덧셈 연산자 -> 문자끼리는 연결할 수 없기에 ASCII 코드로 변환되어 계산됨
+
+            // 이스케이프 문자
+            Console.WriteLine("컴퓨터\t소프트웨어\t공학과"); // tab
+            Console.WriteLine("컴퓨터\n소프트웨어\n공학과"); // 줄 바꿈
+
+            // 문자 선택
+            Console.WriteLine("안녕하세요."[0]); // 안 출력
+            Console.WriteLine("안녕하세요."[1]); // 녕 출력
+
+            // 참과 거짓
+            // Console.WriteLine(52 = 273); // 대입(할당) 연산자
+            Console.WriteLine(52 == 273);
+
+            WriteLine(DateTime.Now.Hour < 3 || 8 < DateTime.Now.Hour); // OR 조건
+            WriteLine(DateTime.Now.Hour < 3 && 8 < DateTime.Now.Hour); // AND 조건
+
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+            Console.WriteLine(c);
+
+            float d = 12.345f;
+            double e = 234.567;
+
+            var g = 10;
+            var h = 123.456;
+        }
+    }
+}
---
+++ 3rd/sln_3/project_1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 복합 대입 연산자
+            int output = 0;
+            output += 52; // output = output + 52;
+            Console.WriteLine(output);
+            output *= 52; // output = output * 52;
+            Console.WriteLine(output);
+
+            //  + (문자열 연결 연산자)
+            String input = "Hello ";
+            input += "World~";
+            Console.WriteLine(input);
+
+            // 증감 연산자
+            int number1 = 10;
+            /*
+            number1++;
+            Console.WriteLine(number1); // 11
+            number1--;
+            Console.WriteLine(number1); // 10
+            ++number1;
+            Console.WriteLine(number1); // 11
+            --number1;
+            Console.WriteLine(number1); // 10
+            */
+
+            int number2 = 20;
+            /*
+            Console.WriteLine(number2); // 20
+            Console.WriteLine(number2++); // 20
+            Console.WriteLine(number2--); // 21
+            Console.WriteLine(number2); // 20
+
+            Console.WriteLine(++number2); // 21
+            Console.WriteLine(--number2); // 20
+            Console.WriteLine(number2); // 20
+            */
+
+            int number3 = 30;
+            /*
+            number3++;
+            number3--;
+            Console.WriteLine(number3); // 30
+
+            ++number3;
+            --number3;
+            Console.WriteLine(number3); // 30
+            */
+
+            // 자료형
+            int _int = 0;
+            long _long = 552273103365;
+            float _float = 52.273f;
+            double _double = 52.273;
+            char _char = '글';
+            String _string = "문자열";
+
+            Console.WriteLine(_int.GetType());
+            Console.WriteLine(_string.GetType());
+
+            // var : 자료형을 자동으로 지정
+            var numberA = 100;
+            var numberB = 100.13F;
+            var numberC = 100.13;
+        }
+    }
+}
