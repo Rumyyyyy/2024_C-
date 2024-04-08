f0bf4a7552a442f88651720f64d2370c371fb033
Rumyyyyy <dkfmadl1219@naver.com>
2024-04-09 08:42:09 +0900

    git commit

---
+++ 5th/sln_5/project_4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score1 = 80;
+            int score2 = 90;
+            int score3 = 85;
+            int average = (score1 + score2 + score3) / 3;
+            Console.WriteLine("average : "+average);
+            Console.WriteLine("average : {0}", average);
+            Console.WriteLine($"average : {average}");
+        }
+    }
+}
